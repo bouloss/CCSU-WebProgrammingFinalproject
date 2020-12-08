396edf9d3dcd4a8686b30037f527315e78c8696b
twoeyedpete <74621917+twoeyedpete@users.noreply.github.com>
2020-12-08 15:36:24 -0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('',views.final, name='final'),
     path('account/',views.account,name='account'),
     path('createt/',views.createt,name='createt'),
-    #path('editt/<str:id>',views.editt,name='editt'),
-    #path('deletet/<str:id>',views.deletet,name='deletet'),
+
+    path('login/',views.loginuser,name='login'),
+    path('logout/',views.logoutuser,name='logout'),
+    path('resource/',views.resource,name='resource')
 ]
--- views.py
+++ views.py
@@ -1,24 +1,13 @@
+from django.contrib.auth import logout, login
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Forum
-from .forms import UserCreationForm
+from .forms import Former
 # Create your views here.
 
 
 def final(request):
     return render(request, 'base.html')
-
-
-def account(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = UserCreationForm()
-        context = {'form',form}
-    return render(request,'account.html',{'form',form})
 
 
 def index(request):
@@ -28,17 +17,48 @@ def index(request):
 
 
 def createt(request):
-    form = Forum(request.POST or None)
+    form = Former(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('index')
-    return render(request,'create_ques',{'form',form})
+    return render(request,'create_ques',{'form':form})
 
 
 def editt(request,id):
     question = Forum.objects.get(id=id)
-    form = Forum(request.POST or None, instance=question)
+    form = Former(request.POST or None, instance=question)
     if form.is_valid():
         form.save()
         return redirect('index')
-    return render(request,'edit_form.html',{'form',form})
+    return render(request,'edit_form.html',{'form':form})
+
+def loginuser(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request,user)
+            redirect('final')
+    else:
+        form = AuthenticationForm()
+
+    return render(request,'login.html', {'form':form})
+
+
+def logoutuser(request):
+    logout(request)
+    return redirect('final')
+
+
+def account(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('final')
+    else:
+        form = UserCreationForm()
+    return render(request,'account.html',{'form':form})
+
+def resource(request):
+    return render(request, 'resource.html')
